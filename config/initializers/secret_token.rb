c124883278301acf45052bdcab64cee1d2827283
Luis Ferreira <hidnasio@gmail.com>
2013-11-09 17:54:50 -0200

    Adds rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Resetme::Application.config.secret_key_base = '3e202603d286ec8654de04aff6a0b2269a62d92ff082bcb0ca7c9577e31350da120715c65fbb6d1de34b46c924da0454629312744d4cc239d02fedb89d3bf3e9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Resetme::Application.config.session_store :cookie_store, key: '_resetme_session'
