a911921703d2eda710d9653eec6edfea009893b1
Luis Ferreira <hidnasio@gmail.com>
2014-02-15 18:19:42 -0200

    Set sites#index as default view

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Resetme::Application.routes.draw do
   resources :sites
 
-  get "welcome/index"
+  get "sites/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'sites#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
