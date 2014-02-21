2d6fcff64355e69930d765f68bbfde35e4c2de25
Luis Ferreira <hidnasio@gmail.com>
2014-02-21 00:21:58 -0200

    Add a default value to reset_at

--- db/migrate/20140221000855_add_reset_at_to_sites.rb
+++ db/migrate/20140221000855_add_reset_at_to_sites.rb
@@ -1,5 +1,8 @@
 class AddResetAtToSites < ActiveRecord::Migration
   def change
     add_column :sites, :reset_at, :datetime
+    reversible do |dir|
+      dir.up { Site.update_all reset_at: DateTime.now }
+    end
   end
 end
